ef7f59fe41815120e62d32d755413248c60e354f
Mohamed Aburehab <mohamed.aburehab@gmail.com>
2022-05-25 14:24:33 +0200

    adding comments to the php code and editing the readme file

--- website/start_flash.php
+++ website/start_flash.php
@@ -1,5 +1,8 @@
 
 <?php
+/*This is the code that is executed when the user sends a request to the web page. 
+  if it doesn't contain the file to does nothing
+  if the file exists it is going to open a file called "lengthPtr.txt" and writing a 0 to it. */
 	if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
 	  if( $_POST['flash'] == 'no_file')
@@ -20,16 +23,28 @@
 
 	}
 
-  $files = glob('uploads/*'); // get all file names
+  /* Getting all the files names in the uploads folder. */
+  $files = glob('uploads/*'); 
+
+/*This is the code that is executed when there is a file in the uploads folder. It is going to open
+  the file lengthPtr.txt and read the first line of it. If the config is set to ok, it is going to
+  return the first line of the file. If the config is set to none, it is going to return the last line
+  of the file. If the config is set to doneBurn, it is going to delete the file and return DONE
+  UPLOAD. If the config is set to allfile, it is going to return all the lines of the file. If the
+  config is set to filelength, it is going to return the length of the file. If the config is not set,
+  it is going to open the file lengthPtr.txt and write a 0 to it. */
   if (is_file($files[0]))
   {
+      /* Getting the first file in the uploads folder and reading it into an array. */
       $path = $files[0];
       $mydata = file($path); //read file in array
       $length = count($mydata);
 
+      /* Checking if the config is set. If it is set, it is going to open the file lengthPtr.txt and read the
+      first line of it. */
       if ( isset( $_GET['config'] ) )
       {
-        /* Open the file */
+        /* Opening a file called lengthPtr.txt and reading the first line of it. */
         $myfile = fopen("lengthPtr.txt","r");
         $ptr =  fgets($myfile);
         fclose($myfile);
@@ -44,6 +59,7 @@
             fclose($myfile);
             //header ("Location: test.php?config=none");
         }
+        /* Returning the last line of the file. */
         elseif ( $_GET['config'] == 'none' && ($ptr < $length) && ($ptr >= 0))
         {
             //echo ($ptr-1);
@@ -59,6 +75,7 @@
             /* Close the file */
             fclose($myfile);
 
+        /* Deleting the file after it is done uploading. */
             if(is_file($files[0]))
             {
               unlink($files[0]); // delete file
@@ -73,12 +90,15 @@
 					}
 				}
 
+      /* Returning the length of the file. */
 				elseif ( $_GET['config'] == 'filelength')
 				{
 					echo $length ;
 				}
 
       }
+      /*  Checking if the config is set. If it is not set, it is going to open the file lengthPtr.txt and
+          write a 0 to it. */
 			else
 			{
 				// config Not set...
@@ -88,6 +108,7 @@
 				fclose($myfile);
 			}
   }
+/* This is the code that is executed when there is no file in the uploads folder. */
   else
   {
       /* No files Found */
--- website/upload.php
+++ website/upload.php
@@ -2,6 +2,7 @@
 
 
 
+  /* Deleting all files in the uploads folder. */
     if(isset($_FILES['fileToUpload']))
     {
     	$files = glob('uploads/*'); // get all file names
@@ -15,26 +16,42 @@
     	}
     }
 
+    /* Creating an array called errors. */
     $errors= array();
+    /* Getting the name of the file. */
     $file_name = $_FILES['fileToUpload']['name'];
+    /* Getting the size of the file. */
     $file_size = $_FILES['fileToUpload']['size'];
+    /* Getting the temporary name of the file. */
     $file_tmp = $_FILES['fileToUpload']['tmp_name'];
+    /* Getting the type of the file. */
     $file_type = $_FILES['fileToUpload']['type'];
+    /* Getting the file extension. */
     $file_ext=strtolower(end(explode('.',$_FILES['fileToUpload']['name'])));
 
+    /* Checking the file extension. */
     $expensions= array("hex");
 
 
+  /* Checking if the file extension is not in the array. */
     if(in_array($file_ext,$expensions)=== false)
     {
       $errors[]="extension not allowed, please choose a HEX file.<br>";
     }
 
+  /* Checking if the file size is greater than 500kb. */
     if($file_size > 500000)
     {
       $errors[]='File size must be excately 500kb';
     }
 
+/*  Checking if there are any errors. If there are no errors, it will move the file to the uploads
+    folder. It will then echo "Success" and echo "The file " + $fileName +  "  has been uploaded."
+    if it has been uploaded.". It will then redirect the user to the done_upload.html page. 
+    
+    if there are any errors it will echo "Sorry, there was an error uploading your file."
+    and then prints the error
+    */
     if(empty($errors)==true)
     {
       move_uploaded_file($file_tmp,"uploads/".$file_name);
@@ -48,5 +65,6 @@
       echo "Sorry, there was an error uploading your file.";
       echo "<br>";
       print_r($errors);
+    /* Closing the if statement. */
     }
 ?>
